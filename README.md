16cf09c039a8dbff945a96e5aea4b6f62a2b72e9
Anthony Rolland <6172844+LaconicPneumonic@users.noreply.github.com>
2020-09-02 13:27:39 -0400

    Add Spousal Information and Backend Scripts (#1)
    * Add css grid layout and hover animations
    
    * Add spousal information
    
    * Update README

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # FamilyTreeViz
 
-I had trouble keeping track of and viewing my family tree, so I made a small app for it.
+I come from a really large family and for years my relatives and I have been trying to record our lineage. One of the things we struggled with was amassing and presenting this information in a way that was easy to update and share. I couldn't find any free or cheap services to do this on the web, so that's when I decide to do it myself.
+
+## GOALS
+- Individuals withing a family, regardless of technical ability, should be able to view their family tree in a simple way.
+- A family should be able to host their app for free (within reason)
 
 ## DESIGN
 TODO
-## GOALS
-TODO
+
 ## HOW TO
 TODO
---
+++ ui/src/components/Person.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="person hover-shadow" @click="selected">
+    <h1>{{this.person.Name}}</h1>
+  </div>
+</template>
+
+<script>
+import DBService from "@/services/DBService.js";
+
+export default {
+  name: "Person",
+  props: {
+    personID: Number,
+    default: -1,
+  },
+  data() {
+    return {
+      person: {
+        Name: "Unloaded",
+        ParentIDs: [],
+        ChildrenIDs: [],
+        PersonID: undefined,
+        SpouseID: undefined
+      },
+    };
+  },
+  async created() {
+    if (this.personID != undefined) {
+      let value = await DBService.GetPersonByID(this.personID);
+      this.person = value[0];
+    }
+  },
+  methods: {
+    selected() {
+      this.$emit("rooted", { PersonID: this.person.PersonID });
+    },
+  },
+};
+</script>
+
+<style>
+.person {
+  background-color: #f0cea0;
+  padding: 0 1.5%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  text-align: center;
+  transition: all 0.6s ease;
+  display: inline-block;
+  box-sizing: border-box;
+  margin: auto 1%;
+}
+
+.person:hover {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.45);
+  transform: translateY(-4px);
+}
+
+.person:active {
+  transform: none;
+}
+
+.person h1 {
+  background-color: #f0cea0;
+  font-family: "Roboto", sans-serif;
+}
+</style>
