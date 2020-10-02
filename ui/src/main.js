fc257928eb59515304769dd70f83f168dc808f0b
Anthony Rolland <6172844+LaconicPneumonic@users.noreply.github.com>
2020-10-02 12:09:02 -0400

    Add Login Flow & Authentication
    * Add authentication view and backend work
    
    * Rename backend file

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,8 +1,49 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import FamilyView from "@/views/FamilyView.vue"
+import LoginView from "@/views/LoginView.vue"
+import DBService from "@/services/DBService.js";
+
+// Initialize icon library
+library.add(faUser, faLock)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+// Add vue router
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '*', redirect: '/' },
+  {
+    path: '/',
+    component: LoginView,
+    name: 'login'
+
+  },
+  {
+    path: '/family',
+    component: FamilyView,
+    name: 'family',
+    beforeEnter: function (_to, _from, next) {
+      if(DBService.store.loggedIn) next()
+      else next("/")
+    }
+  }
+]
+const router = new VueRouter(
+  {
+    routes
+  }
+)
 
 Vue.config.productionTip = false
-
+Vue.prototype.$dbservice = DBService
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
--- ui/src/services/DBService.js
+++ ui/src/services/DBService.js
@@ -1,53 +1,79 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/analytics";
+import "firebase/auth";
 import { firebaseConfig } from "./firebaseConfig.js";
 
-console.log(firebaseConfig);
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 firebase.analytics();
 
 // utils
 const db = firebase.firestore();
+const auth = firebase.auth();
 
 // refs
 const people = db.collection("royalFamily");
 
-function GetPersonByID(personID) {
-    return people
-        .where("PersonID", "==", personID)
-        .get()
-        .then(function (query) {
-            let arr = [];
-            query.forEach(function (doc) {
-                arr.push(doc.data());
-            });
-            return arr;
-        })
-        .catch(function (error) {
-            console.log("Error getting documents: ", error);
-        });
+async function LogInUser(email, password) {
+    try {
+        await auth.setPersistence(firebase.auth.Auth.Persistence.SESSION);
+        return firebase.auth().signInWithEmailAndPassword(email, password);
+    } catch (e) {
+        // we want to obfuscate the reason behind a bad login to hinder brute force attacks
+        return null;
+    }
 }
 
-function GetFirstPerson() {
-    return people
-        .orderBy("PersonID")
-        .limit(1)
-        .get()
-        .then(query => {
-            let arr = [];
-            query.forEach(function (doc) {
-                arr.push(doc.data());
-            });
-            return arr;
-        })
-        .catch(function (error) {
-            console.log("Error getting documents: ", error);
+async function GetPersonByID(personID) {
+    try {
+        const query = await people
+            .where("PersonID", "==", personID)
+            .get();
+        let arr = [];
+        query.forEach(function (doc) {
+            arr.push(doc.data());
         });
+        return arr;
+    } catch (error) {
+        return null;
+    }
 }
 
-// export utils/refs
+async function GetFirstPerson() {
+    try {
+        const query = await people
+            .orderBy("PersonID")
+            .limit(1)
+            .get();
+        let arr = [];
+        query.forEach(function (doc) {
+            arr.push(doc.data());
+        });
+        return arr;
+    } catch (error) {
+        return null;
+    }
+}
+
+var store = {
+    state: {
+        loggedIn: false
+    },
+}
+
+// firebase auth state watcher used because the direct call to auth().currentUser is inaccurate
+auth.onAuthStateChanged(user => {
+    if (user) {
+        store.loggedIn = true;
+    } else {
+        store.loggedIn = false;
+    }
+})
+
 export default {
     GetPersonByID,
-    GetFirstPerson
+    GetFirstPerson,
+    LogInUser,
+    auth,
+    store
 }
